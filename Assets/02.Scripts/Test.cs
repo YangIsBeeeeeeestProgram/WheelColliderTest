318df8a50c0a502f4c76241a0d1f7535ee27213e
YangJiaQi <1295585769@qq.com>
2018-07-18 09:45:46 +0800

    1

---
+++ Assets/02.Scripts/Test.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public WheelCollider[] WheelColliders = new WheelCollider[4];
+    public GameObject[] wheelMeshs = new GameObject[4];
+
+    
+
+}
