b0e07a758b7b6ab7b3627e7f4beb721220b8abbc
YangJiaQi <1295585769@qq.com>
2018-07-19 09:30:37 +0800

    2

--- Assets/02.Scripts/Test.cs
+++ Assets/02.Scripts/Test.cs
@@ -4,9 +4,39 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
-    public WheelCollider[] WheelColliders = new WheelCollider[4];
+    public WheelCollider[] wheelColliders = new WheelCollider[4];
     public GameObject[] wheelMeshs = new GameObject[4];
+    public float maxMotorTorque;
+    public float maxSteeringAngle;
 
-    
+    private void Update()
+    {
+        UpdateMeshsPositions();
+    }
+
+    private void FixedUpdate()
+    {
+        float steer = Input.GetAxis("Horizontal");
+        wheelColliders[0].steerAngle = steer * maxSteeringAngle;
+        wheelColliders[1].steerAngle = steer * maxSteeringAngle;
+        float motor = -Input.GetAxis("Vertical");
+        wheelColliders[2].motorTorque = motor * maxMotorTorque;
+        wheelColliders[3].motorTorque = motor * maxMotorTorque;
+
+    }
+
+    void UpdateMeshsPositions()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            Vector3 pos;
+            Quaternion quat;
+            wheelColliders[i].GetWorldPose(out pos, out quat);
+
+            wheelMeshs[i].transform.position = pos;
+            wheelMeshs[i].transform.rotation = quat;
+        }
+
+    }
 
 }
